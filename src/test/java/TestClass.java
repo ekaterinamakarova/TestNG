d8a0b1222789a4ce7f3d438437a699f9bb4e8c31
Ekaterina Makarova <44766780+ekaterinamakarova@users.noreply.github.com>
2019-08-01 12:14:01 +0300

    Commit

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,9 +1,52 @@
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestClass {
 
-    @Test
+    @Test(priority = 2)
     public void test1(){
-        
+        System.out.println("Test #1");
+
     }
+    @Test(priority = 1, enabled = false)
+    public void test2(){
+        System.out.println("Test #2");
+    }
+
+    @Test(priority = 3, expectedExceptions = NullPointerException.class)
+    public void test3(){
+        Object ref = null;
+        ref.toString();
+        System.out.println("Test #3");
+    }
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("Before suite");
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("After Suite");
+    }
+
+    @BeforeTest
+    public void beforeTest(){
+        System.out.println("Before Test");
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("Before Method");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("After method");
+    }
+
+
+
+
+
+
 }
