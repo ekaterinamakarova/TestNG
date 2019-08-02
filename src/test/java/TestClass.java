94de545042c903b7e3878621ae938a23224c6fe3
Ekaterina Makarova <44766780+ekaterinamakarova@users.noreply.github.com>
2019-08-02 13:16:26 +0300

    Commit

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,6 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TestClass {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.google.ru/?gws_rd=ssl");
+    }
 
     @Test(priority = 2)
     public void test1(){
@@ -39,8 +57,13 @@ public class TestClass {
         System.out.println("Before Method");
     }
 
-    @AfterMethod
-    public void afterMethod(){
+    @AfterTest
+    public void afterMethod() throws IOException {
+        TakesScreenshot scr = ((TakesScreenshot) driver);
+        File file1 = scr.getScreenshotAs(OutputType.FILE);
+
+        FileUtils.copyFile(file1, new File("K:\\test1.PNG"));
+        System.out.println("Screenshot of the test is taken");
         System.out.println("After method");
     }
 
